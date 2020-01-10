728482b1e7c6311e46e821e1e72794149fd0fe59
Nelson Lopez <dev.nelsonlopez@gmail.com>
2020-01-10 12:51:48 -0500

    first commit

---
+++ deck_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card Ace of Spades, but %v", d[0])
+
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected King of clubs but got %v", d[len(d)-1])
+	}
+}
